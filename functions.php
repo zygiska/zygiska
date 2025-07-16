f104ce884fad4159ca1cdd88eb5b423fba259425
zygiska <36248578+zygiska@users.noreply.github.com>
2025-07-16 13:38:53 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'trekky_child_enqueue_styles' );
+testas add_action( 'wp_enqueue_scripts', 'trekky_child_enqueue_styles' );
 function trekky_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
